6710122c669995bc8d886837b4f064ae3c3f5113
Uardet99 <eduardvalls17@gmail.com>
2022-04-11 22:53:50 +0200

    Clases Abstractas

---
+++ Clases_abstractas/abstractas.php
@@ -0,0 +1,15 @@
+<?php
+
+
+abstract class Persona {
+    public function saludo () {
+        return 'Hola';
+    }
+}
+
+class Estudiante extends Persona {
+
+}
+
+$carlos = new Estudiante;
+echo $carlos -> saludo();
