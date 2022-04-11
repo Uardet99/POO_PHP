c9223779619cbdbc94dc04b518a4f861b613cbda
Uardet99 <eduardvalls17@gmail.com>
2022-04-11 23:24:03 +0200

    Cadenas de metodos

---
+++ Cadenas_de_metodos/cadenas_metodos.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class Usuario {
+    public $nombre;
+    public $correo;
+
+    function __construct ($nombre, $correo) {
+        $this -> nombre = $nombre;
+        $this -> correo = $correo;
+    }
+
+    public function mostrarNombre () {
+        echo 'Su nombre es: ' .  $this -> nombre . '<br />';
+        return $this;
+    }
+
+    public function mostrarCorreo () {
+        echo 'Su correo es: ' .  $this -> correo . '<br />';
+        return $this;
+    }
+}
+
+$carlos = new Usuario('Carlos', 'carlos@gmail.com');
+# Para encadenar metodos tendremos que utilizar un return, siempre y cuando pertenezcan al mismo objeto.
+$carlos -> mostrarNombre() -> mostrarCorreo();
